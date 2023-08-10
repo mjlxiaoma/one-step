787b5ecf09b010a6e3663141a0cd5b0839b3cef4
mjl <2075498242@qq.com>
2023-08-10 14:45:32 +0800

    feat

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css';
+import 'ant-design-vue/dist/antd.css';
+import App from './App.vue';
+import { createApp } from 'vue';
+import router from './router/router';
+import plugins from './plugins';
+const app = createApp(App);
+import RunSvg from 'run-svg';
+import 'run-svg/style.css';
+app.use(router).use(plugins).use(RunSvg).mount('#app');
---
+++ src/store/type.ts
@@ -0,0 +1,14 @@
+export interface RouteInfo {
+    name: string;
+    path: string;
+    title: string;
+    icon?: any;
+}
+
+export interface Route {
+    key?: string;
+    path: string;
+    title: string;
+    name: string;
+    icon?: any;
+}
